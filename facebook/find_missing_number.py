9514f2b405e70451ec84567e2bf9236539883bc9
Gerardo Porras <gporras@yahoo-inc.com>
2014-05-27 14:24:48 -0700

    Fisrt file and commit

---
+++ facebook/find_missing_number.py
@@ -0,0 +1,25 @@
+#!/bin/env python
+from collections import defaultdict
+
+#example input 5
+# 1 11 31 41 51
+#out put 21
+
+N = int(raw_input())
+l=raw_input()
+list=[int(elem) for elem in l.split()]
+
+dict={}
+dict = defaultdict(int)
+for i in xrange(N-1):
+    res = list[i+1] - list[i]
+    dict[res] += 1
+
+difference=min(dict, key=dict.get)
+
+for i in xrange(N-1):
+    res = list[i+1] - list[i]
+    if res == difference:
+       print list[i-1] + difference
+       break
+
